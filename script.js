20bc5ddbc9b20fc1ced5a22e4b4a5ca91a23a912
change-coder <80451758+change-coder@users.noreply.github.com>
2021-03-22 10:27:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+(function($) {
+    "use strict";
+     $(document).on('ready', function() {
+	
+       /*=====================================
+			Counter Up JS
+		======================================*/
+		$('.counter').counterUp({
+			delay:20,
+			time:2000
+		});
+		
+		
+	});
+	
+});
